49f24bb50be7177bccfe43a1609004c67137e0ff
Your Name <youremail@huskers.unl.edu>
2021-08-31 04:15:22 +0000

    inicial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Leonardo Meza
+ *         lmeza2@huskers.unl.edu
+ * Date: 2021/08/30
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Leonardo Meza!\n");
+
+  return 0;
+}
+
